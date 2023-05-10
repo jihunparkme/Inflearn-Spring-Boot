c7ea70233c8310bb16dacd5002c5378539f75339
Jihun Park <jihunpark.tech@gmail.com>
2023-05-10 23:09:28 +0900

    [Spring Boot] Auto Configuration: @Conditional

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,22 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * MemoryCondition matches() 실행
+ * 결과가 true 일 경우
+ * MemoryConfig 는 정상 동작 -> memoryController, memoryFinder 빈 등록
+ *
+ * 결과가 false 일 경우
+ * MemoryConfig 는 무효화 -> memoryController, memoryFinder 빈은 등록되지 않음
+ */
 @Configuration
+@Conditional(MemoryCondition.class)
 public class MemoryConfig {
     @Bean
     public MemoryController memoryController() {
