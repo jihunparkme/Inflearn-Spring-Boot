ff3346ef865bb25af7a06ead43727364fbf540e6
Jihun Park <jihunpark.tech@gmail.com>
2023-06-20 09:10:57 +0900

    [SpringBoot] 메트릭 등록 - Timer

---
+++ actuator/src/main/java/hello/order/v3/OrderServiceV3.java
@@ -0,0 +1,62 @@
+package hello.order.v3;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class OrderServiceV3 implements OrderService {
+    private final MeterRegistry registry;
+    private AtomicInteger stock = new AtomicInteger(100);
+
+    public OrderServiceV3(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Override
+    public void order() {
+        Timer timer = Timer.builder("my.order") // 메트릭 이름을 지정하여 타이머 생성
+                .tag("class", this.getClass().getName()) // 프로메테우스에서 필터할 수 있는 레이블로 사용
+                .tag("method", "order")
+                .description("order")
+                .register(registry); // 만든 타이머를 MeterRegistry 에 등록
+
+        timer.record(() -> {
+            log.info("주문");
+            stock.decrementAndGet();
+            sleep(500);
+        });
+    }
+
+    @Override
+    public void cancel() {
+        Timer timer = Timer.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "cancel")
+                .description("order")
+                .register(registry);
+
+        timer.record(() -> {
+            log.info("취소");
+            stock.incrementAndGet();
+            sleep(200);
+        });
+    }
+
+    private static void sleep(int l) {
+        try {
+            Thread.sleep(l + new Random().nextInt(200));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+}
